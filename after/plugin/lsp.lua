455e5da8ca8033c031eebcdfd38384371eabf860
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2023-01-21 17:25:01 +0700

    fix(lsp): solve luasnip jumpable not work

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -33,8 +33,28 @@ end)
 
 
 local cmp = require('cmp')
+local luasnip = require("luasnip")
+
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-Space>'] = cmp.mapping.complete()
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<Tab>'] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
+    else 
+      fallback()
+    end
+  end, { "i", "s" }),
+  ['<S-Tab>'] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else 
+      fallback()
+    end
+  end, { "i", "s" })
 })
 
 lsp.setup_nvim_cmp({
