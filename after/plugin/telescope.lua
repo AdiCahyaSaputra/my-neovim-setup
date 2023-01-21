a51b7ac92420755f3dd28f6b4c4e8c75f9acad71
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2023-01-21 21:10:15 +0700

    perf(telescope): ignore node_modules, .git, and vendor

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local ok, builtin = pcall(require, 'telescope.builtin')
 if not ok then return end
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>ff', '<cmd>lua require"telescope.builtin".find_files(require("telescope.themes").get_dropdown({ previewer = false, hidden = true, file_ignore_patterns = {".git", "vendor", "node_modules"} }))<cr>')
 
 local telescope = require'telescope'
 
