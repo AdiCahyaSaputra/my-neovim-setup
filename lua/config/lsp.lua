28974818943488add42777528d535bf9db89e941
Adi Cahya Saputra <adi.jsonparse@gmail.com>
2023-10-13 17:57:06 +0700

    fix: broken change

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -3,8 +3,6 @@ if not ok then
   return
 end
 
-lsp.preset("recommended")
-
 lsp.on_attach(function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
@@ -40,51 +38,29 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ["<C-Space>"] = cmp.mapping.complete(),
-  }),
-})
-
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ["<C-Space>"] = cmp.mapping.complete(),
-  ["<Tab>"] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
-      luasnip.expand_or_jump()
-    else
-      fallback()
-    end
-  end, { "i", "s" }),
-  ["<S-Tab>"] = cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_prev_item()
-    elseif luasnip.jumpable(-1) then
-      luasnip.jump(-1)
-    else
-      fallback()
-    end
-  end, { "i", "s" }),
-  ["<CR>"] = cmp.mapping.confirm({
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = true,
-  }),
-})
-
-lsp.setup_nvim_cmp({
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "path" },
-    { name = "buffer" },
-    { name = "luasnip" },
-    { name = "nvim_lsp_signature_help" },
-  },
-  mapping = cmp_mappings,
-  formatting = {
-    format = require("lspkind").cmp_format({
-      mode = "symbol_text",
-      maxwidth = 50,
-      ellipsis_char = "...",
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
     }),
-  },
+  }),
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
